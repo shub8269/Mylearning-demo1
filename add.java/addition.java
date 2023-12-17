4dead0d80e1625562e354d7c4a625515ac4a7a43
shub8269 <shub8269@gmail.com>
2023-12-17 15:28:52 +0530

    java repository

---
+++ add.java/addition.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class addition
+{
+    public static void main(String args[])
+    {   
+        int a = 10 , b = 20 ;
+        System.out.println("added no is"+ (a+b));
+    }
+}
